b60bc9063a05c7dcf87afcb2cce6f2704e2cdd6c
katosun2 <katosun2>
2022-03-28 16:39:54 +0800

    添加右键

--- 添加nvim到右键.bat
+++ 添加nvim到右键.bat
@@ -1,8 +1,12 @@
 @echo off
+
+set curpath=%~dp0
+set nvimbin="%curpath%..\..\bin\nvim-qt.exe"
+
 echo 设置 nvim 右键
 reg add "HKEY_CLASSES_ROOT\*\shell\Edit with vim" /v "" /t REG_SZ /d "用 Neovim 编辑" /f
-reg add "HKEY_CLASSES_ROOT\*\shell\Edit with vim" /v "Icon" /t REG_SZ /d "D:\nvim\bin\nvim-qt.exe,0" /f
-reg add "HKEY_CLASSES_ROOT\*\shell\Edit with vim\command" /v "" /t REG_SZ /d "D:\nvim\bin\nvim-qt.exe --no-ext-tabline \"%%1\"" /f
+reg add "HKEY_CLASSES_ROOT\*\shell\Edit with vim" /v "Icon" /t REG_SZ /d "%nvimbin%,0" /f
+reg add "HKEY_CLASSES_ROOT\*\shell\Edit with vim\command" /v "" /t REG_SZ /d "%nvimbin% --no-ext-tabline \"%%1\"" /f
 echo nvim 右键设置完成 
 echo 如果设置失败，请使用管理员权限运行！
 pause
