2231ff73bfd84810e5ed608e21ea3e57e27e070c
katosun2 <katosun2>
2022-03-30 08:58:31 +0800

    快捷键优化

--- 添加nvim到右键.bat
+++ 添加nvim到右键.bat
@@ -4,7 +4,7 @@ set curpath=%~dp0
 set nvimbin="%curpath%..\..\bin\nvim-qt.exe"
 
 echo 设置 nvim 右键
-reg add "HKEY_CLASSES_ROOT\*\shell\Edit with vim" /v "" /t REG_SZ /d "用 Neovim 编辑" /f
+reg add "HKEY_CLASSES_ROOT\*\shell\Edit with vim" /v "" /t REG_SZ /d "用 Neovim 编辑(&N)" /f
 reg add "HKEY_CLASSES_ROOT\*\shell\Edit with vim" /v "Icon" /t REG_SZ /d "%nvimbin%,0" /f
 reg add "HKEY_CLASSES_ROOT\*\shell\Edit with vim\command" /v "" /t REG_SZ /d "%nvimbin% --no-ext-tabline \"%%1\"" /f
 echo nvim 右键设置完成 
