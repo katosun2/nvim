080ed175c0a1fe49d3d5f7cd9ea26cb336ced0c7
katosun2 <katosun2>
2022-04-11 09:56:42 +0800

    fixe 閰嶇疆

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -1,6 +1,6 @@
 require'lspconfig'.vuels.setup{}
 require'lspconfig'.tsserver.setup{}
---require'lspconfig'.dartls.setup{}
+require'lspconfig'.dartls.setup{}
 require'lspconfig'.html.setup{}
 require'lspconfig'.cssls.setup{}
 require'lspconfig'.cssmodules_ls.setup{}
@@ -8,4 +8,4 @@ require'lspconfig'.yamlls.setup{}
 require'lspconfig'.jsonls.setup{}
 require'lspconfig'.dockerls.setup{}
 require'lspconfig'.vimls.setup{}
-require("flutter-tools").setup{}
+--require("flutter-tools").setup{}
