519d32c219d46bc83a3997368e7bfa5aae776aa3
unknown <katosun2>
2025-07-26 17:12:16 +0800

    doc

---
+++ install_modules.bat
@@ -0,0 +1,14 @@
+:: ÉèÖÃ±àÂë
+chcp 65001
+@echo off
+npm install pynvim -g
+npm install tree-sitter@0.25.0 tree-sitter-cli@0.25.8 -g
+npm install -g vls
+npm install -g typescript typescript-language-server
+npm install -g vscode-langservers-extracted
+npm install -g cssmodules-language-server
+npm install -g yaml-language-server
+npm install -g dockerfile-language-server-nodejs
+npm install -g vim-language-server
+pause
+@echo on
